3e46a856cee07d0c010d4b5be05706f342b5ae7e
lisa.leonowa@yandex.ru <lisa.leonowa@yandex.ru>
2024-02-14 20:04:53 +0300

    HomeWork lesson 5 (Update Homework lesson 4)

--- DjangoLess2/Lesson2/forms.py
+++ DjangoLess2/Lesson2/forms.py
@@ -6,21 +6,34 @@ class ClientForm(ModelForm):
     class Meta:
         model = ClientModel
         fields = ('name', 'email', 'phone', 'address')
+        labels = {'name': 'Наименование',
+                  'email': 'Почта',
+                  'phone': 'Номер телефона',
+                  'address': 'Адрес'}
 
 
 class GoodForm(ModelForm):
     class Meta:
         model = GoodModel
         fields = ('name', 'description', 'price_good', 'quantity_good', 'image')
+        labels = {'name': 'Наименование',
+                  'description': 'Описание',
+                  'price_good': 'Стоимость',
+                  'quantity_good': 'Кол-во осталось',
+                  'image': 'Изображение'}
 
 
 class OrderForm(ModelForm):
     class Meta:
         model = OrderModel
         fields = ('id_client',)
+        labels = {'id_client': 'Клиент'}
 
 
 class OrderListForm(ModelForm):
     class Meta:
         model = OrderListModel
         fields = ('id_good', 'id_order', 'count')
+        labels = {'id_good': 'Товар',
+                  'id_order': 'Заказ',
+                  'count': 'Кол-во товара'}
